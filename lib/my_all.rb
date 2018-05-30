718e8775d75022cdb2e67f81f8d5122015fe851f
Richard Fenix <richard.fenix@gmail.com>
2018-05-30 03:33:20 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -6,7 +6,7 @@ def my_all?(collection)
   while i < collection.length 
     block_return_values << yield(collection[i])
     bining.pry
-    i +=
+    i = i + 1
   end
 
   if block_return_values.include?(false)
